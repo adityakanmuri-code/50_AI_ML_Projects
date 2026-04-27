3e2f7e16c9a71ef89c1448599c255fd92b8c5c1d
Aditya <adityakanmuri@gmail.com>
2026-04-27 14:31:44 +0530

    Added Config File

---
+++ config/configuration.py
@@ -0,0 +1,13 @@
+import yaml
+import os
+
+class Config:
+    def __init__(self,config_path="config/config.yaml"):
+        with open(config_path,"r") as file:
+            self.config =yaml.safe_load(file)
+    
+    def get(self,*keys):
+        value = self.config
+        for key in keys:
+            value = value[key]
+        return value
