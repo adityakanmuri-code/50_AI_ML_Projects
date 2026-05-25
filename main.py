e933a2ad3a94d29247459803d820fbee816a0f7b
Aditya <adityakanmuri@gmail.com>
2026-05-25 22:16:02 +0530

    First Commit for Amazon Reviews Sentimment Analysis

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-import Customer_Churn_Model_ANN.logger as logger
+import Amazon_Reviews_Sentimment_Analysis.logger as logger
+import logging
 
-from Customer_Churn_Model_ANN.data_ingestion import DataIngestion
-from Customer_Churn_Model_ANN.eda_data_transformation import CleanData,EDA
-from Customer_Churn_Model_ANN.model_training import Model_Trainer
-from Customer_Churn_Model_ANN.model_prediction import Prediction
 
 def main():
-    ingestion = DataIngestion(data_type='train')
-    df = ingestion.ingest_data()
-
-    #print(df.head())
-    #print(df['Geography'].value_counts())
-    #print(df['Gender'].value_counts())
-
-    cleaner = CleanData()
-    df = cleaner.clean_data(df)
-    df = cleaner.transform_data(df)
-    #eda = EDA(df)
-    #eda.plot_data()
-
-    trainer = Model_Trainer(df)
-    trainer.model_trainer()
-
-    predict = Prediction()
-    predict.predict_data()
+    logging.info("Amazon Reviews Check Pass")
 
 if __name__ == "__main__":
     main()
