cf4073d7cc09895b6e66be2566d26e6ca44e16df
Aditya <adityakanmuri@gmail.com>
2026-05-27 22:12:18 +0530

    Added few steps for text preprocessing

--- Amazon_Reviews_Sentimment_Analysis/text_preprocessing.py
+++ Amazon_Reviews_Sentimment_Analysis/text_preprocessing.py
@@ -6,3 +6,48 @@ import logging
 import warnings
 warnings.filterwarnings('ignore')
 from Amazon_Reviews_Sentimment_Analysis.config.configuration import Config
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import gensim
+from gensim.models import Word2Vec
+import re
+import pandas as pd
+import emoji
+
+class Text_Preprocessing:
+    def __init__(self):
+        self.config = Config()
+        self.text_preprocess_steps = self.config.get('transformation','text_preprocessing','preprocess_steps')
+
+    def preprocess_text(self,dataframe:pd.DataFrame = None):
+        try:
+            logging.info('______________________________')
+            logging.info('Text Preprocessing has started')
+            logging.info('______________________________')
+            for step in self.text_preprocess_steps:
+                step_name = step['name']
+                step_params = step.get('params',{})
+                if not hasattr(self,step_name):
+                    error_message = f'{step_name} is not a valid step'
+                    logging.info(error_message)
+                    raise AttributeError(error_message)
+                method = getattr(self,step_name)
+                dataframe = method(dataframe,**step_params)
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+    def lower_text(self,dataframe:pd.DataFrame = None,col:str = None):
+        try:
+            dataframe[col] = dataframe[col].str.lower()
+            return dataframe
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+    def remove_special_characters(self,dataframe:pd.DataFrame = None,col:str = None,search_pattern:str=None):
+        try:
+            dataframe[col] = dataframe[col].apply(lambda x : re.sub(search_pattern," ",str(x)))
+            print(dataframe[col].head())
+            return dataframe
+        except Exception as e:
+            raise CustomException(e,sys)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from Amazon_Reviews_Sentimment_Analysis.data_ingestion import DataIngestion
 from Amazon_Reviews_Sentimment_Analysis.eda_data_transformation import CleanData
+from Amazon_Reviews_Sentimment_Analysis.text_preprocessing import Text_Preprocessing
 
 
 def main():
@@ -11,5 +12,7 @@ def main():
     senti_df = cleaner.clean_data(senti_df)
     senti_df = cleaner.transform_data(senti_df)
 
+    text_preprocess = Text_Preprocessing()
+    text_preprocess.preprocess_text(senti_df)
 if __name__ == "__main__":
     main()
